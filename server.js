2700b625c17b85737eaf2a7cd92a9135e3a20a69
andom-0001 <anandkumar241103@gmail.com>
2026-04-13 22:04:44 +0530

    update .env file and server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import resumeRoutes from "./routes/resumeRoutes.js";
 import { connectDB } from "./config/db.js";
+
 const app = express();
 
 connectDB();
